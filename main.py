90597af2cb24a89f45cdd44ab5461ea460c0856a
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-07-01 11:43:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,82 @@
+# Queue Implementation
+class MyCircularQueue:
+    def __init__(self, k):
+        self.size = 0
+        self.max_size = k
+        self.front = 0
+        self.rear = -1
+        self.queue = [0] * k
+
+    def enQueue(self, value):
+        if self.isFull():
+            return False
+        else:
+            self.rear = (self.rear + 1) % self.max_size
+            self.queue[self.rear] = value
+            self.size += 1
+            return True
+        
+
+    def deQueue(self):
+        if self.isEmpty():
+            return False
+        else:
+            self.front = (self.front+1) % self.max_size
+            # self.queue[self.front]=0
+            self.size -= 1
+            return True
+        
+
+    def Front(self):
+        return self.queue[self.front] if self.size else -1
+        
+
+    def Rear(self):
+        return self.queue[self.rear] if self.size else -1
+        
+
+    def isEmpty(self) :
+        return self.size == 0
+        
+
+    def isFull(self) :
+        return self.size == self.max_size
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+obj = MyCircularQueue(6)
+param_1 = obj.enQueue(3)
+
+param_2 = obj.deQueue()
+
+param_3 = obj.Front()
+param_4 = obj.Rear()
+param_5 = obj.isEmpty()
+param_6 = obj.isFull()
+print(obj.queue)
+
+# Arrange coins
+class Solution(object):
+    def arrangeCoins(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count=0
+        index=1
+        while(n>=index):
+            count+=1
+            n=n-index
+            index=index+1
+        return count
+class Solutions():
+    def arrangeCoins(self, n):
+        if(n==0):
+            return 0
+        current=1
+        rem = n-1
+        while(rem>=current+1):
+            current+=1
+            rem-=current
+          
+        return current
