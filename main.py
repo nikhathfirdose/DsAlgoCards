cf60518ab03b074cf9415b4d57451e0ca6499c31
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-07-01 11:47:00 +0000

    Queue: CircularQueue

--- main.py
+++ main.py
@@ -55,28 +55,5 @@ param_5 = obj.isEmpty()
 param_6 = obj.isFull()
 print(obj.queue)
 
-# Arrange coins
-class Solution(object):
-    def arrangeCoins(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        count=0
-        index=1
-        while(n>=index):
-            count+=1
-            n=n-index
-            index=index+1
-        return count
-class Solutions():
-    def arrangeCoins(self, n):
-        if(n==0):
-            return 0
-        current=1
-        rem = n-1
-        while(rem>=current+1):
-            current+=1
-            rem-=current
-          
-        return current
+
+# checkingg repl
