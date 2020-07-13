981324e6ad7dd65454744c99babb0a03e0c4cd6f
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-07-13 16:00:39 +0000

    Polymorphism

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-# Polymorphism
-
-class ProgLanguage:
+class Employee:
   def __init__(self, name):
     self.name = name
+    print(f'{self.name} is an employee')
 
-class JavaScript(ProgLanguage):
-  def comment(self):
-    print(f'// is a comment in {self.name}')
+class Manager(Employee):
+  def __init__(self, department, name):
+    self.department = department
+    self.name = name
+    # super().__init__(name)
+    # print(super())
+    print(f'Manager, {self.department} department')
 
-class Python(ProgLanguage):
-  def comment(self):
-    print(f'# is a comment in {self.name}')
+staff_1 = Manager('HR', 'Andy')
+print(dir(staff_1))
+class Developer:
+  def __init__(self, name, language):
+    self.name = name
+    self.language = language
 
+  def introduce(self):
+    print(f'Hi! I am {self.name}. I code in {self.language}')
 
-obj1 = JavaScript("JavaScript")
-obj2= Python("Python")
+dev = Developer('Matt', 'Python')
 
-for i in [obj1,obj2]:
-  i.comment()
-
-obj1.comment()
-obj2.comment()
+print(dir(dev))
